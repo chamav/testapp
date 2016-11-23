3383b0cb7181bf670b3e5f0b56e1d4796552be60
Vladimir Chemeris <chamav@gmail.com>
2016-11-24 00:09:33 +0700

    jwt

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,5 +52,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        //JWT
+        'jwt-auth' => \App\Http\Middleware\AuthJWT::class,
     ];
 }
